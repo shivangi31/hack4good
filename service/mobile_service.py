cd7c81c944822fcc0bee165caea317038894224a
Rahul Bansal <rabans@microsoft.com>
2018-07-23 21:03:41 +0530

    Added code for user registreation

--- service/mobile_service.py
+++ service/mobile_service.py
@@ -1,7 +1,16 @@
 class MobileService:
-    def __init__(self):
-        # self.db=db
+    def __init__(self,db):
+        self.mongo=db
         pass
+
+    @staticmethod
+    def registerUser(firstName, lastName, age, sex, pincode, college, email, password):
+        user = {"firstName":firstName,"lastName":lastName, "age":age, "sex":sex, "pincode":pincode, "colege":college, "email":email, "password":password}
+        user_id = self.mongo.user.insert_one(user)
+        user["id"] = str(recipe_id.inserted_id)
+        return user
+
+
 
     @staticmethod
     def getQuestions():
