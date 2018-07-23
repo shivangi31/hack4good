3417d609343a857b514567e3328cce68c24f7bb0
Rahul Bansal <rabans@microsoft.com>
2018-07-23 20:52:53 +0530

    Added the pincode map

--- main.py
+++ main.py
@@ -6,14 +6,20 @@ from flask_pymongo import PyMongo
 app = Flask(__name__)
 
 from service.mobile_service import MobileService
+from util.util_service import UtilService
 
 app.config["MONGO_URI"] = "mongodb://localhost:27017/rabans"
 mongo = PyMongo(app)
+PinCodePath = "./resources/IN.txt"
+pincode = {}
 
 # @app.before_request
 # def init_mongo():
 #     g.mongo = MongoClient(ConfigService.get_mongo_host(), 27017).rabans
 
+
+def initPinCode():
+    pincode =  UtilService.pinCodeParser(PinCodePath)
 
 @app.route('/mobile/questions', methods = ['GET'])
 def getQuestions():
@@ -28,4 +34,5 @@ def hello():
     return 'Hello, World!'
 
 if __name__ == "__main__":
+    initPinCode()
     app.run(debug=True)
